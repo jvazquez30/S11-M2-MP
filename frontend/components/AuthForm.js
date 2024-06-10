7f7f4af74d1ba6df2e4c219c89fa4dc979b1ec57
jvazquez30 <josevazquez881@gmail.com>
2024-06-09 17:34:00 -0700

    added logout and login security

--- frontend/components/AuthForm.js
+++ frontend/components/AuthForm.js
@@ -1,4 +1,7 @@
-import React, { useState } from 'react'
+import React, { useState, } from 'react'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
+
 
 export default function AuthForm() {
   const [isLogin, setIsLogin] = useState(true)
@@ -6,6 +9,7 @@ export default function AuthForm() {
   const [password, setPassword] = useState('')
   const [error, setError] = useState('')
   const [message, setMessage] = useState('')
+  const navigate = useNavigate('')
 
   const toggleFormMode = () => {
     setIsLogin(!isLogin)
@@ -19,6 +23,31 @@ export default function AuthForm() {
     setPassword(event.target.value)
   }
 
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setError('')
+    setMessage('')
+    try {
+      const { data } = await axios.post(
+        `/api/auth/${isLogin ? 'login' : 'register'}`,
+        { username, password}
+      )
+      if (isLogin) {
+        localStorage.setItem("token", data.token)
+        navigate('/stars')
+      } else {
+        setMessage(data.message)
+      }
+    } catch (error) {
+      setError(error?.response?.data?.message
+        || 
+        'An error occured. Please try again.'
+      )
+    }
+
+  }
+  
+
   return (
     <div className="container">
       <div aria-live="polite">{message}</div>
@@ -28,7 +57,7 @@ export default function AuthForm() {
           Switch to {isLogin ? 'Register' : 'Login'}
         </button>
       </h3>
-      <form>
+      <form onSubmit={onSubmit}>
         <div>
           <label htmlFor="username">Username:</label>
           <input
--- frontend/components/StarsList.js
+++ frontend/components/StarsList.js
@@ -1,10 +1,41 @@
 import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
+
 
 export default function StarsList() {
   const [stars, setStars] = useState([])
+  const navigate = useNavigate('')
+
+
+  const logOut = () => {
+    localStorage.removeItem('token')
+    navigate('/')
+  }
+
+  useEffect(() => {
+    const token = localStorage.getItem('token')
+    if (!token) {
+      navigate('/')
+    } else {
+      const getStars = async () => {
+        try {
+          const response = await axios.get(
+            '/api/stars',
+            { headers: { Authorization: token } }
+          )
+          setStars(response.data)
+        } catch (error) {
+          if (error?.response?.status == 401) logOut()
+        }
+      }
+      getStars()
+    }
+  }, [])
+
   return (
     <div className="container">
-      <h3>StarsList <button>Logout</button></h3>
+      <h3>StarsList <button onClick={logOut}>Logout</button></h3>
       {stars.length > 0 ? (
         <div>
           {stars.map((star) => (
